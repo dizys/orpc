b2bc7e72d96ade7ccb3c0f8403247fffe16b20ae
KingJ <501315555@qq.com>
2018-11-29 10:53:45 +0800

    Add test

---
+++ test/gateway.test.ts
@@ -0,0 +1,78 @@
+import {RPCSchema, RPCServer, ServicePrototype} from '../bld/library';
+
+import {createClient, createGateway, createServer} from './utils';
+
+export interface HelloServicePrototype extends ServicePrototype {
+  say(name: string): string;
+  getKeys(object: object): string[];
+  wrapData(name: string, age: number, gender: string): object;
+  bufferEcho(body: Buffer): Buffer;
+  sayPromise(name: string): Promise<string>;
+  error(message: string): never;
+}
+
+export interface IMyRPC extends RPCSchema {
+  hello: HelloServicePrototype;
+}
+
+class HelloService implements HelloServicePrototype {
+  say(name: string): string {
+    return `Hello, ${name}!`;
+  }
+
+  getKeys(object: object): string[] {
+    return Object.keys(object);
+  }
+
+  wrapData(name: string, age: number, gender: string): object {
+    return {
+      name,
+      age,
+      gender,
+    };
+  }
+
+  bufferEcho(body: Buffer): Buffer {
+    return body;
+  }
+
+  error(message: string): never {
+    throw new Error(message);
+  }
+
+  sayPromise(name: string): Promise<string> {
+    return new Promise<string>(resolve => {
+      resolve(`Hello, ${name}!`);
+    });
+  }
+}
+
+let myRPC: IMyRPC = {
+  hello: new HelloService(),
+};
+
+let rpc = createClient<IMyRPC>(8014);
+
+let server: RPCServer;
+
+createServer(myRPC, 2015)
+  .then(value => (server = value))
+  .catch(console.error);
+
+let gateway = createGateway();
+
+test('simplest service method call', async () => {
+  let result = await rpc.hello.say('Dizy');
+
+  expect(result).toBe('Hello, Dizy!');
+});
+
+afterAll(() => {
+  rpc.$portal.close();
+
+  if (server) {
+    server.stop();
+  }
+
+  gateway.stop();
+});
--- test/utils.ts
+++ test/utils.ts
@@ -1,18 +1,37 @@
 import {
+  Gateway,
+  GatewayConfig,
   RPCClient,
   RPCSchema,
   RPCServer,
   createClient as _createClient,
 } from '../bld/library';
 
-export async function createServer(schemaImpl: any): Promise<RPCServer> {
+export async function createServer(
+  schemaImpl: any,
+  port: number = 8013,
+): Promise<RPCServer> {
   let server = new RPCServer(schemaImpl);
 
-  server.start(8013);
+  server.start(port);
 
   return server;
 }
 
-export function createClient<Schema extends RPCSchema>(): RPCClient<Schema> {
-  return _createClient<Schema>('http://localhost:8013/');
+export function createClient<Schema extends RPCSchema>(
+  port: number = 8013,
+): RPCClient<Schema> {
+  return _createClient<Schema>(`http://localhost:${port}/`);
+}
+
+export function createGateway(): Gateway {
+  let config: GatewayConfig = {
+    servers: [{url: 'http://localhost:8015/', weight: 1}],
+  };
+
+  let gateway = new Gateway(config);
+
+  gateway.start(8014);
+
+  return gateway;
 }
